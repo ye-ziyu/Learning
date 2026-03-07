92d872f26ffc7ca6d48b63eeac6387e41296aff8
1171332956@qq.com <15082956+yehy_1_0@user.noreply.gitee.com>
2026-03-07 20:57:37 +0800

    RAG技术

---
+++ alibaba/RAG/src/main/java/ai/learn/rag/RagApplication.java
@@ -0,0 +1,15 @@
+package ai.learn.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RagApplication.class, args);
+    }
+
+}
